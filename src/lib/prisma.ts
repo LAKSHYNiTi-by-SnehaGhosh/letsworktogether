a59763a4cb540df4fdc085113f5f0df453247710
LAKSHYNiTi <contact.lakshyniti@gmail.com>
2026-07-31 16:51:02 +0530

    fix(prisma): safe prisma initialization when DATABASE_URL is not present during build step

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,21 +2,29 @@ import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL || "";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
-  prisma = new PrismaClient({ adapter });
-} else {
-  if (!globalForPrisma.prisma) {
+  if (connectionString) {
     const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
-    globalForPrisma.prisma = new PrismaClient({ adapter });
+    prisma = new PrismaClient({ adapter });
+  } else {
+    prisma = new PrismaClient();
+  }
+} else {
+  if (!globalForPrisma.prisma) {
+    if (connectionString) {
+      const pool = new Pool({ connectionString });
+      const adapter = new PrismaPg(pool);
+      globalForPrisma.prisma = new PrismaClient({ adapter });
+    } else {
+      globalForPrisma.prisma = new PrismaClient();
+    }
   }
   prisma = globalForPrisma.prisma;
 }
